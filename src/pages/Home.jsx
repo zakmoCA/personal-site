78bc067ec7d087dd11fbd8164956827460c20b09
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-15 04:15:45 +1000

    add plan and scroll settings to home component

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import { Suspense } from 'react'
+import { useState, Suspense } from 'react'
 import Navbar from '../components/Navbar'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
@@ -11,6 +11,8 @@ import Plane from '../models/Plane'
   POPUP
 </div> */}
 const Home = () => {
+  const [isRotating, setIsRotating] = useState(false)
+
   const adjustIslandForScreenSize = () => {
     let screenScale = null
     let screenPosition = [0, -6.5, -43]
@@ -24,13 +26,29 @@ const Home = () => {
 
     return [screenScale, screenPosition, rotation]
   }
+  
+  const adjustPlaneForScreenSize = () => {
+    let screenScale, screenPosition
+
+    if (window.innerWidth < 768) {
+      screenScale = [1.5, 1.5, 1.5]
+      screenPosition = [0, -1.5, 0]
+    } else {
+      screenScale = [3, 3, 3]
+      screenPosition = [0, -4, -4]
+    }
+
+    return [screenScale, screenPosition]
+  }
 
   const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
+  const [planeScale, planePosition] = adjustPlaneForScreenSize()
 
   return (
     <section className="w-full h-screen relative">
       <Canvas 
-        className="w-full h-screen bg-transparent"
+        className={`w-full h-screen bg-transparent ${isRotating ? 
+        'cursor-grabbing' : 'cursor-grab'}`}
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
@@ -44,8 +62,15 @@ const Home = () => {
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
+            isRotating={isRotating}
+            setIsRotating={setIsRotating}
           />
-          <Plane />
+          <Plane 
+            isRotating={isRotating}
+            planeScale={planeScale}
+            planePosition={planePosition}
+            rotation={[0, 20, 0]}
+          />
         </Suspense>
       </Canvas>
     </section>
