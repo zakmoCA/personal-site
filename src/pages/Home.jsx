1ca9211e12558f396122b2093fbbcbbaa906ead2
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-15 00:12:27 +1000

    rotation, lighting, sky

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../components/Navbar'
 import { Canvas } from '@react-three/fiber'
 import Loader from '../components/Loader'
 import Island from '../models/Island'
+import Sky from '../models/Sky'
 
 {/* <div className="absolute top-28 left-0 right-0 z-10 flex items-center justify-center">
   POPUP
@@ -10,16 +11,19 @@ import Island from '../models/Island'
 const Home = () => {
   const adjustIslandForScreenSize = () => {
     let screenScale = null
-    let screenPosition 
+    let screenPosition = [0, -6.5, -43]
+    let rotation = [0.1, 4.7, 0] // experiment with best config
 
     if (window.innerWidth < 768) {
       screenScale = [0.9, 0.9, 0.9]
-      screenPosition = [0, -6.5, -43]
     } else {
       screenScale = [1, 1, 1]
-      screenPosition = [0, -6.5, -43]
     }
+
+    return [screenScale, screenPosition, rotation]
   }
+
+  const [islandScale, islandPosition, islandRotation] = adjustIslandForScreenSize()
 
   return (
     <section className="w-full h-screen relative">
@@ -28,13 +32,16 @@ const Home = () => {
         camera={{ near: 0.1, far: 1000 }}
       >
         <Suspense fallback={<Loader />}>
-          <directionalLight />
-          <ambientLight />
-          <pointLight />
-          <spotLight />
-          <hemisphereLight />
+          <directionalLight position={[1, 1, 1]} intensity={2} />
+          <ambientLight intensity={0.5} />
+          <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1} />
 
-          <Island />
+          <Sky />
+          <Island 
+            position={islandPosition}
+            scale={islandScale}
+            rotation={islandRotation}
+          />
         </Suspense>
       </Canvas>
     </section>
