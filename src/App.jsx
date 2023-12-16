5312df19c7735208bfdddd2747511984cb97b726
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-17 00:13:46 +1000

    under construction sign added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Sign from './components/Sign'
 import { Home, About, Projects, Contact } from './pages'
 
 const App = () => {
@@ -7,6 +8,7 @@ const App = () => {
     <main className="bg-slate-300/20">
       <Router>
         <Navbar /> 
+        <Sign />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
---
+++ src/components/Sign.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import sign from '../assets/images/under_construction.jpeg'
+
+const Sign = () => {
+  return (
+    <div className="flex justify-center">
+      <img src={sign} alt="sign"  />
+    </div>
+  )
+}
+
+export default Sign
