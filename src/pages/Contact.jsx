b9f480784dad6568142b02d3f39d70f105b14332
zakmoCA <zakeriyaamohamed@gmail.com>
2023-12-19 03:36:45 +1000

    cooking with gas baby emailjs LFG

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,14 +1,50 @@
 import { useRef, useState } from 'react'
+import emailjs from '@emailjs/browser'
 
 const Contact = () => {
   const formRef = useRef(null)
-  const [form, setform] = useState({ name: '', email: '', message: ''})
+  const [form, setForm] = useState({ name: '', email: '', message: ''})
   const [isLoading, setIsLoading] = useState(false)
 
-  const handleChange = () => {}
-  const handleFocus = () => {}
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value})
+  }
+
+  const handleFocus = () => {
+
+  }
+
   const handleBlur = () => {}
-  const handleSubmit = () => {}
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    setIsLoading(true)
+
+    const email = import.meta.env.VITE_APP_MY_EMAIL
+
+
+    emailjs.sendForm(
+      import.meta.env.VITE_APP_EMAILJS_SERVICE_ID,
+      import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
+      formRef.current,
+      // {
+      //   from_name: form.name,
+      //   to_name: 'Zak',
+      //   from_email: form.email,
+      //   to_email: email,
+      //   message: form.message
+      // },
+      import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
+    ).then(() => {
+      setIsLoading(false)
+      // TODO: show success msg
+      // TODO: hide an alert
+    }).catch((error) => {
+      setIsLoading(false)
+      console.log(error)
+      // TODO: show error msg
+    })
+  }
 
   return (
     <section className="relative-flex lg:flex-row flex-col max-container">
@@ -17,8 +53,10 @@ const Contact = () => {
 
         <form
           className="w-full flex flex-col gap-7 mt-14"
+          ref={formRef}
           onSubmit={handleSubmit}
-        />
+        >
+        
           <label className="text-black-500 font-semibold">
             Name 
             <input
@@ -66,9 +104,11 @@ const Contact = () => {
             disabled={isLoading}
             onFocus={handleFocus}
             onBlur={handleBlur}
+            onSubmit={handleSubmit}
           >
             {isLoading ? 'Sending...' : 'Send message'}
           </button>
+        </form>
       </div>
     </section>
   )
